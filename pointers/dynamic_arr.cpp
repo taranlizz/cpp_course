d1d261a82a9f9eb7f4e28b2b4b57ee2087e93f49
Yelyzaveta Taran <lizataran103@gmail.com>
2024-12-08 17:04:40 +0200

    dynamic arr based on input

--- pointers/dynamic_arr.cpp
+++ pointers/dynamic_arr.cpp
@@ -7,42 +7,42 @@ using namespace std;
 
 int main()
 {
-    size_t size_arr{10};
+    // size_t size_arr{10};
 
-    // Different ways you can declare an array
-    // dynamically and how they are initialized
+    // // Different ways you can declare an array
+    // // dynamically and how they are initialized
 
-    double *p_salaries = new double[size_arr];                        // array will contain garbage values
-    int *p_students = new (nothrow) int[size_arr]{};                  // all values are initialized to 0
-    double *p_scores = new (nothrow) double[size_arr]{1, 2, 3, 4, 5}; // first five values will be initialized, and the rest will be 0's.
+    // double *p_salaries = new double[size_arr];                        // array will contain garbage values
+    // int *p_students = new (nothrow) int[size_arr]{};                  // all values are initialized to 0
+    // double *p_scores = new (nothrow) double[size_arr]{1, 2, 3, 4, 5}; // first five values will be initialized, and the rest will be 0's.
 
-    if (p_scores)
-    {
-        for (size_t i{}; i < size_arr; i++)
-        {
-            cout << "value : " << p_scores[i] << " : " << *(p_scores + i) << endl;
-        }
-    }
+    // if (p_scores)
+    // {
+    //     for (size_t i{}; i < size_arr; i++)
+    //     {
+    //         cout << "value : " << p_scores[i] << " : " << *(p_scores + i) << endl;
+    //     }
+    // }
 
-    delete[] p_scores;
-    p_scores = nullptr;
+    // delete[] p_scores;
+    // p_scores = nullptr;
 
-    delete[] p_students;
-    p_students = nullptr;
+    // delete[] p_students;
+    // p_students = nullptr;
 
-    delete[] p_salaries;
-    p_salaries = nullptr;
+    // delete[] p_salaries;
+    // p_salaries = nullptr;
 
     // Static arrays vs dynamic arrays
 
-    int scores[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    cout << "scores size: " << size(scores) << endl; // lives on the stack
-    for (int s : scores)
-    {
-        cout << "value : " << s << std::endl;
-    }
+    // int scores[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // cout << "scores size: " << size(scores) << endl; // lives on the stack
+    // for (int s : scores)
+    // {
+    //     cout << "value : " << s << std::endl;
+    // }
 
-    int *p_scores1 = new int[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // lives on the heap;
+    // int *p_scores1 = new int[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // lives on the heap;
 
     // cout << "p_scores size: " << size(p_scores) << endl; // ERROR
     // for (int s : p_scores)
@@ -51,4 +51,18 @@ int main()
     // } // ERROR
 
     // A dynamic array decays to pointer
+
+    int size = 0;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int *p_arr = new int[size];
+    for (size_t i{}; i < size; i++)
+    {
+        p_arr[i] = i;
+        cout << i << " : " << p_arr[i] << endl;
+    }
+
+    delete p_arr;
+    p_arr = nullptr;
 }
