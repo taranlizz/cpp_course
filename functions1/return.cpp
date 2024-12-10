167be3ea0042fc0f429d5ff81d221a6a382d4c27
Yelyzaveta Taran <lizataran103@gmail.com>
2024-12-10 06:25:30 +0200

    function overloading

--- functions1/return.cpp
+++ functions1/return.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
+// #include <iostream>
 
-using std::string, std::cout, std::endl;
+// using std::string, std::cout, std::endl;
 
-int sum(int a, int b);
+// int sum(int a, int b);
 
-string concat(string str1, string str2);
+// string concat(string str1, string str2);
 
-int main()
-{
-    // int a = 24;
-    // int b = 26;
-    // int result = sum(a, b);
+// int main()
+// {
+//     // int a = 24;
+//     // int b = 26;
+//     // int result = sum(a, b);
 
-    // cout << "result: " << result << endl;
+//     // cout << "result: " << result << endl;
 
-    string result = concat(string{"Hello"}, string{", World!"});
-    cout << "Out : &result: " << &result << endl;
-    cout << "result: " << result << endl;
-}
+//     string result = concat(string{"Hello"}, string{", World!"});
+//     cout << "Out : &result: " << &result << endl;
+//     cout << "result: " << result << endl;
+// }
 
-int sum(int a, int b)
-{
-    return a + b;
-}
+// int sum(int a, int b)
+// {
+//     return a + b;
+// }
 
-string concat(string str1, string str2)
-{
-    string result = str1 + str2;
-    cout << "In : &result : " << &result << endl;
-    return result;
-}
+// string concat(string str1, string str2)
+// {
+//     string result = str1 + str2;
+//     cout << "In : &result : " << &result << endl;
+//     return result;
+// }
